509b4390757f3d070a4c6092992ad172d0b01805
syswj <838260772@qq.com>
2014-06-27 17:04:29 +0800

    0626

--- 0626/1-1chat/chat1.c
+++ 0626/1-1chat/chat1.c
@@ -10,30 +10,44 @@
 #include<sys/stat.h>
 #include<sys/types.h>
 #include<stdlib.h>
+#include<signal.h>
 #include<string.h>
+void handle(int num)
+{
+wait();
+exit(0);
+}
 int main()
 {
 	int fd_w,fd_r;
 	char buf[1024];
+	pid_t pid;
 	fd_w=open("1_2.fifo",O_WRONLY);//双方打开两个管道的顺序应该一样，
 	fd_r=open("2_1.fifo",O_RDONLY);
   printf("fd_r: %d   fd_w : %d\n",fd_r,fd_w);
-	if(fork()>0)
+	if((pid=fork())>0)
 	{//父进程负责写管道1
 		close(fd_r);
 		FILE *fp=fdopen(fd_w,"w");
+		signal(17,handle);//注册17号信号量，子进程结束后发来17号信号量，
 		while(memset(buf,0,1024),fgets(buf,1024,stdin)!=NULL)
 		{
 			fprintf(fp,"from 1: %s",buf);
 			fflush(fp);
 		}
 		close(fd_w);
+        kill(pid,2);
+        fprintf(fp,"bye",NULL);
+//		while(1);
 	}else
 	{//子进程读管道2
 		close(fd_w);
 		while(memset(buf,0,1024),read(fd_r,buf,1024)>0)
 		{
+			if(strncmp("bye",buf,3)!=0)
 			write(1,buf,strlen(buf));
+			else
+				break;
 		}
 		close(fd_r);
 	}
--- 0626/1-1chat/chat2.c
+++ 0626/1-1chat/chat2.c
@@ -8,26 +8,41 @@
 #include<stdio.h>
 #include<sys/stat.h>
 #include<sys/types.h>
+#include<signal.h>
 #include<fcntl.h>
 #include<stdlib.h>
 #include<string.h>
+void handler(int num)
+{
+wait();
+exit(0);
+}
 int main()
 {
 	int fd_w,fd_r;
 	char buf[1024];
+	pid_t pid;
 	fd_r=open("1_2.fifo",O_RDONLY);
 	fd_w=open("2_1.fifo",O_WRONLY);
    printf("fd_r: %d   fd_w: %d\n",fd_r,fd_w);
-	if(fork()>0)
+	if((pid=fork())> 0)
 	{//父进程负责读管道1
 		close(fd_w);
+		signal(17,handler);
 		while(memset(buf,0,1024),read(fd_r,buf,1024)>0)
 		{
+			if(strncmp("bye",buf,3)==0)
+				break;
+			else
+			{	
 			write(1,buf,strlen(buf));
 			fflush(fdopen(fd_r,"r"));
+			}
 		}
+         kill(pid,2);
 		close(fd_r);
-		wait();
+//		wait();
+//		while(1);
 	}else
 	{//子进程负责写管道2
 		close(fd_r);
@@ -37,6 +52,7 @@ int main()
 			fprintf(fp,"from 2: %s",buf);
 			fflush(fp);
 		}
+		fprintf(fp,"bye",NULL);
 		close(fd_w);
 	}
 	return 0;
